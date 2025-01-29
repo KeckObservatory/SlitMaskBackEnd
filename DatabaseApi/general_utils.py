f64701b0ddfa9052484b718a5aa7ad34d2805c50
= <=>
2025-01-29 08:22:43 -1000

    changed displayed Mask Name to GUI Name for observer tables

--- DatabaseApi/general_utils.py
+++ DatabaseApi/general_utils.py
@@ -491,7 +491,7 @@ def order_inventory(results):
     new_keys_map = [
         ('status', 'Status'), ('desdate', 'Design-Date'),
         ('date_use', 'Date-Use'), ('stamp', 'Submitted'),
-        ('projname', 'Project-Name'), ('guiname', 'Mask-Name'),
+        ('projname', 'Project-Name'), ('guiname', 'GUI-Name'),
         ('maskid', 'Barcode'),
         ('desname', 'Design-Name'),
         ('desnslit', 'Number-Slits'), ('instrume', 'Instrument'),
